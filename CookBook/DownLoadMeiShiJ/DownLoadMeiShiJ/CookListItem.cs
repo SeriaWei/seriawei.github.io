fa44ac586db541d22bef91e9fdbcc0c6eba0edc2
Wayne <seriawei@outlook.com>
2015-02-05 20:55:15 +0800

    example data

---
+++ CookBook/DownLoadMeiShiJ/DownLoadMeiShiJ/CookListItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DownLoadMeiShiJ
+{
+    public class CookListItem
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string DetailUrl { get; set; }
+        public string DetailID
+        {
+            get
+            {
+                if (CategoryReName != null && DetailUrl != null)
+                {
+                    return CategoryReName + "/detail/" + DetailUrl.Substring(DetailUrl.LastIndexOf("/") + 1).Replace(".html", ".json");
+                }
+                return string.Empty;
+            }
+        }
+        public string CategoryName { get; set; }
+        public string CategoryReName { get; set; }
+
+    }
+}
--- CookBook/DownLoadMeiShiJ/DownLoadMeiShiJ/Program.cs
+++ CookBook/DownLoadMeiShiJ/DownLoadMeiShiJ/Program.cs
@@ -13,6 +13,7 @@ namespace DownLoadMeiShiJ
         {
             XmlDocument doc = new XmlDocument();
             doc.Load("DataSource.xml");
+            string favePath = doc.GetElementsByTagName("Categories")[0].Attributes["savepath"].InnerText;
             XmlNodeList categories = doc.GetElementsByTagName("Category");
             foreach (XmlNode category in categories)
             {
@@ -23,8 +24,41 @@ namespace DownLoadMeiShiJ
                     string itemname = item.Attributes["name"].InnerText;
                     string itemrename = item.Attributes["rename"].InnerText;
                     string itemurl = item.Attributes["url"].InnerText;
-                    HtmlWeb web=new HtmlWeb();
-                    web.Load(itemurl);
+                    int maxpage = 0;
+                    int.TryParse(item.Attributes["maxpage"].InnerText, out maxpage);
+                    for (int i = 1; i <= maxpage; i++)
+                    {
+                        HtmlWeb web = new HtmlWeb();
+                        HtmlDocument htmldoc = web.Load(itemurl + "?&?&page=" + i);
+                        HtmlNodeCollection cookItems = htmldoc.DocumentNode.SelectNodes("//*[@id=\"listtyle1_list\"]/div");
+                        List<CookListItem> pageItems = new List<CookListItem>();
+                        foreach (HtmlNode cookItem in cookItems)
+                        {
+                            CookListItem cItem = new CookListItem();
+                            HtmlNode href = cookItem.SelectSingleNode("a");
+                            cItem.DetailUrl = href.GetAttributeValue("href", "");
+                            cItem.Name = href.GetAttributeValue("title", "");
+                            cItem.ImageUrl = cookItem.SelectSingleNode("a/img").GetAttributeValue("src", "");
+                            cItem.CategoryName = itemname;
+                            cItem.CategoryReName = itemrename;
+                            pageItems.Add(cItem);
+                        }
+                        string result = Newtonsoft.Json.JsonConvert.SerializeObject(pageItems);
+                        string dir = favePath + rename + "\\";
+                        if (!System.IO.Directory.Exists(dir))
+                        {
+                            System.IO.Directory.CreateDirectory(dir);
+                        }
+                        string file = dir + itemrename + "_p" + i + ".json";
+                        Console.WriteLine(file);
+                        System.IO.FileStream fs = new System.IO.FileStream(file, System.IO.FileMode.Create);
+                        System.IO.StreamWriter writer = new System.IO.StreamWriter(fs);
+                        writer.Write(result);
+                        writer.Close();
+                        writer.Dispose();
+                        fs.Close();
+                        fs.Dispose();
+                    }
 
                 }
             }
