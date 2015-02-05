6f6b940dbca1b59d0d2bb5a1eb4c1567f8de70cd
Wayne <seriawei@gmail.com>
2015-02-05 17:19:55 +0800

    DownLoadMeishij

---
+++ CookBook/DownLoadMeiShiJ/DownLoadMeiShiJ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using HtmlAgilityPack;
+
+namespace DownLoadMeiShiJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("DataSource.xml");
+            XmlNodeList categories = doc.GetElementsByTagName("Category");
+            foreach (XmlNode category in categories)
+            {
+                string name = category.Attributes["name"].InnerText;
+                string rename = category.Attributes["rename"].InnerText;
+                foreach (XmlNode item in category.ChildNodes)
+                {
+                    string itemname = item.Attributes["name"].InnerText;
+                    string itemrename = item.Attributes["rename"].InnerText;
+                    string itemurl = item.Attributes["url"].InnerText;
+                    HtmlWeb web=new HtmlWeb();
+                    web.Load(itemurl);
+
+                }
+            }
+        }
+    }
+}
