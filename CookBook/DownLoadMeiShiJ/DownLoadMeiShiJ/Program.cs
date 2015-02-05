0337a46eb097e72d4ca7d3b639a9f2ea34a674f0
Wayne <seriawei@outlook.com>
2015-02-05 21:34:12 +0800

    exampledata

--- CookBook/DownLoadMeiShiJ/DownLoadMeiShiJ/Program.cs
+++ CookBook/DownLoadMeiShiJ/DownLoadMeiShiJ/Program.cs
@@ -43,7 +43,7 @@ namespace DownLoadMeiShiJ
                             cItem.CategoryReName = itemrename;
                             pageItems.Add(cItem);
                         }
-                        string result = Newtonsoft.Json.JsonConvert.SerializeObject(pageItems);
+                        string result = Newtonsoft.Json.JsonConvert.SerializeObject(pageItems);                        
                         string dir = favePath + rename + "\\";
                         if (!System.IO.Directory.Exists(dir))
                         {
@@ -53,7 +53,7 @@ namespace DownLoadMeiShiJ
                         Console.WriteLine(file);
                         System.IO.FileStream fs = new System.IO.FileStream(file, System.IO.FileMode.Create);
                         System.IO.StreamWriter writer = new System.IO.StreamWriter(fs);
-                        writer.Write(result);
+                        writer.Write(Encoding.UTF8.GetString(Encoding.Default.GetBytes(result)));
                         writer.Close();
                         writer.Dispose();
                         fs.Close();
