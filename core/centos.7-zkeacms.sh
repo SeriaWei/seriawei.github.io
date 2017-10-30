1427f769076a79f63ea384fae531d72542f57e31
Wayne <seriawei@outlook.com>
2017-10-30 16:06:24 +0800

    .

--- core/centos.7-zkeacms.sh
+++ core/centos.7-zkeacms.sh
@@ -1,5 +1,3 @@
-#!/bin/bash2
-
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
 sudo sh -c 'echo -e "[packages-microsoft-com-prod]\nname=packages-microsoft-com-prod \nbaseurl=https://packages.microsoft.com/yumrepos/microsoft-rhel7.3-prod\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/dotnetdev.repo' &&
 sudo yum update -y &&
