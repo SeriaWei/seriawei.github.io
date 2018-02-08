9dd85f7ecce052253f532d5fcd867ae48dc4f2ca
Wayne <seriawei@outlook.com>
2018-02-08 09:18:39 +0800

    linux.aspnetcore.sh

--- core/linux.aspnetcore.sh
+++ core/linux.aspnetcore.sh
@@ -1,3 +1,6 @@
+rm -rf dotnet-runtime.tar.gz && 
+rm -rf aspnetcore-store.tar.gz &&
+rm -rf dotnet &&
 wget -O dotnet-runtime.tar.gz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-linux-x64.tar.gz &&
 wget -O aspnetcore-store.tar.gz https://download.microsoft.com/download/5/C/1/5C190037-632B-443D-842D-39085F02E1E8/aspnetcore-store-2.0.3-linux-x64.tar.gz &&
 mkdir dotnet &&
