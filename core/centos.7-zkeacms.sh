adbc7aac6098783a7a346da5d39844c010335d14
Wayne <seriawei@outlook.com>
2018-02-08 11:16:27 +0800

    simply deploy

--- core/centos.7-zkeacms.sh
+++ core/centos.7-zkeacms.sh
@@ -1,22 +1,10 @@
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-sudo sh -c 'echo -e "[packages-microsoft-com-prod]\nname=packages-microsoft-com-prod \nbaseurl=https://packages.microsoft.com/yumrepos/microsoft-rhel7.3-prod\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/dotnetdev.repo' &&
-sudo yum update -y &&
-sudo yum install libunwind libicu -y &&
-sudo yum install dotnet-sdk-2.1.4 -y &&
-sudo yum install zip unzip -y &&
-sudo rpm -ivh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm &&
-sudo yum install nginx -y &&
-sudo wget -O cms.zip http://cdn.zkeasoft.com/core/cms.zip &&
-sudo mkdir /zkeasoft &&
-sudo mkdir /zkeasoft/cms &&
-sudo unzip cms.zip -d /zkeasoft/cms &&
-sudo wget http://cdn.zkeasoft.com/core/nginx-zkeacms.conf &&
-sudo wget http://cdn.zkeasoft.com/core/zkeacms.service &&
-sudo mv nginx-zkeacms.conf /etc/nginx/conf.d/zkeacms.conf -f &&
-sudo mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
-sudo mv /etc/nginx/conf.d/default.conf backup-nginx-default.conf &&
-sudo systemctl start nginx &&
-sudo systemctl start zkeacms &&
-sudo systemctl enable nginx &&
-sudo systemctl enable zkeacms &&
-sudo systemctl restart nginx
+curl http://cdn.zkeasoft.com/core/linux.aspnetcore.sh|sh &&
+
+yum install zip unzip -y &&
+wget -O cms.zip http://cdn.zkeasoft.com/core/cms.zip &&
+mkdir -p /zkeasoft/cms
+unzip cms.zip -d /zkeasoft/cms &&
+wget http://cdn.zkeasoft.com/core/zkeacms.service &&
+mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
+systemctl start zkeacms &&
+systemctl enable zkeacms
--- core/ubuntu.16.04-zkeacms.sh
+++ core/ubuntu.16.04-zkeacms.sh
@@ -1,21 +1,10 @@
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &&
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg &&
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list' &&
-sudo apt-get update -y &&
-sudo apt-get install dotnet-sdk-2.1.4 -y &&
-sudo apt-get install zip unzip -y &&
-sudo apt-get install nginx -y &&
-sudo wget -O cms.zip http://cdn.zkeasoft.com/core/cms.zip &&
-sudo mkdir /zkeasoft -p &&
-sudo mkdir /zkeasoft/cms -p &&
-sudo unzip cms.zip -d /zkeasoft/cms &&
-sudo wget http://cdn.zkeasoft.com/core/nginx-zkeacms.conf &&
-sudo wget http://cdn.zkeasoft.com/core/zkeacms.service &&
-sudo mv nginx-zkeacms.conf /etc/nginx/conf.d/zkeacms.conf -f &&
-sudo mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
-sudo mv /etc/nginx/sites-available/default backup-nginx-default -f &&
-sudo systemctl start nginx &&
-sudo systemctl start zkeacms &&
-sudo systemctl enable nginx &&
-sudo systemctl enable zkeacms &&
-sudo systemctl restart nginx
+curl http://cdn.zkeasoft.com/core/linux.aspnetcore.sh|sh &&
+
+apt-get install zip unzip -y &&
+wget -O cms.zip http://cdn.zkeasoft.com/core/cms.zip &&
+mkdir -p /zkeasoft/cms
+unzip cms.zip -d /zkeasoft/cms &&
+wget http://cdn.zkeasoft.com/core/zkeacms.service &&
+mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
+systemctl start zkeacms &&
+systemctl enable zkeacms
