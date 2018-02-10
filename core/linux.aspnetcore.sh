fc789138cf3081bf08e37eaad311f3597093c763
Wayne <seriawei@outlook.com>
2018-02-10 09:17:43 +0800

    .

--- core/linux.aspnetcore.sh
+++ core/linux.aspnetcore.sh
@@ -1,6 +1,10 @@
-rm -rf dotnet.tar.gz && 
+rm -rf dotnet-runtime-linux-x64.tar.gz &&
+rm -rf aspnetcore-linux-x64.tar.gz &&
 rm -rf /zkeasoft/dotnet &&
 mkdir -p /zkeasoft/dotnet &&
-wget -O dotnet.tar.gz https://download.microsoft.com/download/1/1/5/115B762D-2B41-4AF3-9A63-92D9680B9409/dotnet-sdk-2.1.4-linux-x64.tar.gz &&
-tar zxf dotnet.tar.gz -C /zkeasoft/dotnet &&
-rm -rf dotnet.tar.gz
+wget -O dotnet-runtime-linux-x64.tar.gz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-linux-x64.tar.gz &&
+tar zxf dotnet-runtime-linux-x64.tar.gz -C /zkeasoft/dotnet &&
+rm -rf dotnet-runtime-linux-x64.tar.gz &&
+wget -O aspnetcore-linux-x64.tar.gz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/aspnetcore-store-2.0.5-linux-x64.tar.gz &&
+tar zxf aspnetcore-linux-x64.tar.gz -C /zkeasoft/dotnet &&
+rm -rf aspnetcore-linux-x64.tar.gz
