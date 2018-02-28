03ad6b29b18cefcffd0e0c6300d2ec843068e6a5
Wayne <seriawei@outlook.com>
2018-02-28 16:15:32 +0800

    IIS check

--- core/win-zkeacms.ps1
+++ core/win-zkeacms.ps1
@@ -1,32 +1,53 @@
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 Import-Module WebAdministration
 $WebPath=[environment]::CurrentDirectory + "\ZKEACMS";
+Write-Host "Welcome to use ZKEACMS, visit our website(http://www.zkea.net) for more information"
 Write-Host "ZKEACMS will install to $WebPath"
+Write-Host "Installation started. Press Ctrl+C to stop."
+Write-Host "Checking IIS status..."
+$iis = Get-Service IISADMIN -ErrorAction Ignore
+if($iis){
+    if($iis.Status -eq "Running") {
+        Write-Host "IIS Service is running"
+    }
+    else {
+        Write-Host "IIS Service is not running"
+    }
+}
+else {
+	Write-Host "IIS Service not installed"
+	exit 0;
+}
 
-if(!(Get-Command dotnet -ErrorAction SilentlyContinue))
+Write-Host "Downloading DotNetCore.WindowsHosting."
+if(Test-Path -Path "DotNetCore.WindowsHosting.exe")
 {
-	Write-Host "Downloading DotNetCore.WindowsHosting."
-	#Invoke-WebRequest -Uri "https://download.microsoft.com/download/6/A/A/6AA4EDFF-645B-48C5-81CC-ED5963AEAD48/vc_redist.x64.exe" -OutFile "vc_redist.x64.exe"
-	Invoke-WebRequest -Uri "https://aka.ms/dotnetcore.2.0.0-windowshosting" -OutFile "DotNetCore.WindowsHosting.exe"
-	#Start-Process "vc_redist.x64.exe" -Wait 
-	Start-Process "DotNetCore.WindowsHosting.exe" -Wait
-
-	#Remove-Item -Path "vc_redist.x64.exe" -Force 
 	Remove-Item -Path "DotNetCore.WindowsHosting.exe" -Force
 }
+Invoke-WebRequest -Uri "https://aka.ms/dotnetcore.2.0.0-windowshosting" -OutFile "DotNetCore.WindowsHosting.exe"
+
+Write-Host "Installing DotNetCore.WindowsHosting."
+Start-Process "DotNetCore.WindowsHosting.exe" -Wait
+if(Test-Path -Path "DotNetCore.WindowsHosting.exe")
+{
+	Remove-Item -Path "DotNetCore.WindowsHosting.exe" -Force
+}
+
+Write-Host "Downloading ZKEACMS application package."
 if(Test-Path -Path "ZKEACMS.zip")
 {
 	Remove-Item -Path "ZKEACMS.zip" -Force
 }
-Write-Host "Downloading ZKEACMS application package."
 Invoke-WebRequest -Uri "http://cdn.zkeasoft.com/core/cms.zip" -OutFile "ZKEACMS.zip"
+
+Write-Host "Unzip ZKEACMS application package."
 if(Test-Path "ZKEACMS")
 {
 	Remove-Item -Path "ZKEACMS" -Force -Recurse
 }
-Write-Host "Unzip ZKEACMS application package."
 [System.IO.Compression.ZipFile]::ExtractToDirectory("ZKEACMS.zip" ,"ZKEACMS")
-#set-executionpolicy remotesigned
+
+Write-Host "Setting up IIS."
 if(!(Test-Path IIS:\AppPools\ZKEACMS))
 {
 	New-Item -path IIS:\AppPools\ZKEACMS
