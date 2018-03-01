660d04b06fd4a154cf508ec4adee15ae42807be9
Wayne <seriawei@outlook.com>
2018-03-01 17:10:52 +0800

    sudo

--- core/linux.aspnetcore.sh
+++ core/linux.aspnetcore.sh
@@ -1,10 +1,10 @@
-rm -rf dotnet-runtime-linux-x64.tar.gz &&
-rm -rf aspnetcore-linux-x64.tar.gz &&
-rm -rf /zkeasoft/dotnet &&
-mkdir -p /zkeasoft/dotnet &&
-wget -O dotnet-runtime-linux-x64.tar.gz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-linux-x64.tar.gz &&
-tar zxf dotnet-runtime-linux-x64.tar.gz -C /zkeasoft/dotnet &&
-rm -rf dotnet-runtime-linux-x64.tar.gz &&
-wget -O aspnetcore-linux-x64.tar.gz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/aspnetcore-store-2.0.5-linux-x64.tar.gz &&
-tar zxf aspnetcore-linux-x64.tar.gz -C /zkeasoft/dotnet &&
-rm -rf aspnetcore-linux-x64.tar.gz
+sudo rm -rf dotnet-runtime-linux-x64.tar.gz &&
+sudo rm -rf aspnetcore-linux-x64.tar.gz &&
+sudo rm -rf /zkeasoft/dotnet &&
+sudo mkdir -p /zkeasoft/dotnet &&
+sudo wget -O dotnet-runtime-linux-x64.tar.gz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-linux-x64.tar.gz &&
+sudo tar zxf dotnet-runtime-linux-x64.tar.gz -C /zkeasoft/dotnet &&
+sudo rm -rf dotnet-runtime-linux-x64.tar.gz &&
+sudo wget -O aspnetcore-linux-x64.tar.gz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/aspnetcore-store-2.0.5-linux-x64.tar.gz &&
+sudo tar zxf aspnetcore-linux-x64.tar.gz -C /zkeasoft/dotnet &&
+sudo rm -rf aspnetcore-linux-x64.tar.gz
--- core/ubuntu.16.04-zkeacms.sh
+++ core/ubuntu.16.04-zkeacms.sh
@@ -1,11 +1,11 @@
-curl http://cdn.zkeasoft.com/core/linux.aspnetcore.sh|sh &&
-apt-get update -y &&
-apt-get install libunwind-dev -y &&
-apt-get install zip unzip -y &&
-wget -O cms.zip http://cdn.zkeasoft.com/core/cms.zip &&
-mkdir -p /zkeasoft/cms
-unzip cms.zip -d /zkeasoft/cms &&
-wget http://cdn.zkeasoft.com/core/zkeacms.service &&
-mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
-systemctl start zkeacms &&
-systemctl enable zkeacms
+sudo curl http://cdn.zkeasoft.com/core/linux.aspnetcore.sh|sh &&
+sudo apt-get update -y &&
+sudo apt-get install libunwind-dev -y &&
+sudo apt-get install zip unzip -y &&
+sudo wget -O cms.zip http://cdn.zkeasoft.com/core/cms.zip &&
+sudo mkdir -p /zkeasoft/cms
+sudo unzip cms.zip -d /zkeasoft/cms &&
+sudo wget http://cdn.zkeasoft.com/core/zkeacms.service &&
+sudo mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
+sudo systemctl start zkeacms &&
+sudo systemctl enable zkeacms
