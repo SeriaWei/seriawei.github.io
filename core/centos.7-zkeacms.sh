7e6dbc9ea09f1972889c308ca270d482d51c91d7
Wayne <seriawei@outlook.com>
2018-03-01 17:16:19 +0800

    sudo

--- core/centos.7-zkeacms.sh
+++ core/centos.7-zkeacms.sh
@@ -1,11 +1,11 @@
-curl http://cdn.zkeasoft.com/core/linux.aspnetcore.sh|sh &&
-yum check-update -y &&
-yum install libunwind libicu -y &&
-yum install zip unzip -y &&
-wget -O cms.zip http://cdn.zkeasoft.com/core/cms.zip &&
-mkdir -p /zkeasoft/cms
-unzip cms.zip -d /zkeasoft/cms &&
-wget http://cdn.zkeasoft.com/core/zkeacms.service &&
-mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
-systemctl start zkeacms &&
-systemctl enable zkeacms
+sudo curl http://cdn.zkeasoft.com/core/linux.aspnetcore.sh|sh &&
+sudo yum check-update -y &&
+sudo yum install libunwind libicu -y &&
+sudo yum install zip unzip -y &&
+sudo wget -O cms.zip http://cdn.zkeasoft.com/core/cms.zip &&
+sudo mkdir -p /zkeasoft/cms
+sudo unzip cms.zip -d /zkeasoft/cms &&
+sudo wget http://cdn.zkeasoft.com/core/zkeacms.service &&
+sudo mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
+sudo systemctl start zkeacms &&
+sudo systemctl enable zkeacms
