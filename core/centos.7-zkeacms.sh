05066ecc1e87678d7b13d38d0c4bb21c577a416b
Wayne <seriawei@outlook.com>
2018-03-02 11:17:49 +0800

    http://cdn.zkeasoft.com/core/cms.zip -> http://zkeasoft-10041459.file.myqcloud.com/core/cms.zip

--- core/centos.7-zkeacms.sh
+++ core/centos.7-zkeacms.sh
@@ -2,7 +2,7 @@ sudo curl http://cdn.zkeasoft.com/core/linux.aspnetcore.sh|sh &&
 sudo yum check-update -y &&
 sudo yum install libunwind libicu -y &&
 sudo yum install zip unzip -y &&
-sudo wget -O cms.zip http://cdn.zkeasoft.com/core/cms.zip &&
+sudo wget -O cms.zip http://zkeasoft-10041459.file.myqcloud.com/core/cms.zip &&
 sudo mkdir -p /zkeasoft/cms
 sudo unzip cms.zip -d /zkeasoft/cms &&
 sudo wget http://cdn.zkeasoft.com/core/zkeacms.service &&
