b06b2186175b5c0bcc02969ffef522bfd1955355
Wayne <seriawei@outlook.com>
2018-03-20 13:17:29 +0800

    overwrite files WITHOUT prompting

--- core/ubuntu.16.04-zkeacms.sh
+++ core/ubuntu.16.04-zkeacms.sh
@@ -5,7 +5,7 @@ sudo apt-get install zip unzip -y &&
 sudo rm -rf cms.zip &&
 sudo wget -O cms.zip http://zkeasoft-10041459.file.myqcloud.com/core/cms.zip &&
 sudo mkdir -p /zkeasoft/cms
-sudo unzip cms.zip -d /zkeasoft/cms &&
+sudo unzip -o cms.zip -d /zkeasoft/cms &&
 sudo wget http://cdn.zkeasoft.com/core/zkeacms.service &&
 sudo mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
 sudo systemctl start zkeacms &&
