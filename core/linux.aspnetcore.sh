d487c5e9ce2f3eb6058bf5fe07d108b7870f8a05
Wayne <seriawei@outlook.com>
2018-03-21 15:52:28 +0800

    Update

--- core/linux.aspnetcore.sh
+++ core/linux.aspnetcore.sh
@@ -1,9 +1,10 @@
-echo Downloading dotnet-hosting &&
-sudo rm -rf dotnet-hosting.tar.gz &&
-sudo rm -rf /zkeasoft/dotnet &&
-sudo mkdir -p /zkeasoft/dotnet &&
-sudo wget -O dotnet-hosting.tar.gz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-hosting-2.0.5-linux-x64.tar.gz &&
-sudo tar zxf dotnet-hosting.tar.gz -C /zkeasoft/dotnet &&
+#!/bin/sh
+echo Downloading dotnet-hosting
+sudo rm -rf dotnet-hosting.tar.gz
+sudo rm -rf /zkeasoft/dotnet 
+sudo mkdir -p /zkeasoft/dotnet
+sudo wget -O dotnet-hosting.tar.gz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-hosting-2.0.5-linux-x64.tar.gz
+sudo tar zxf dotnet-hosting.tar.gz -C /zkeasoft/dotnet
 sudo rm -rf dotnet-hosting.tar.gz
 
 
--- core/ubuntu.16.04-zkeacms.sh
+++ core/ubuntu.16.04-zkeacms.sh
@@ -1,13 +1,14 @@
-sudo curl http://cdn.zkeasoft.com/core/linux.aspnetcore.sh|sh &&
-sudo apt-get update -y &&
-sudo apt-get install libunwind-dev -y &&
-sudo apt-get install zip unzip -y &&
-sudo rm -rf cms.zip &&
-echo Downloading ZKEACMS package &&
-sudo wget -O cms.zip http://zkeasoft-10041459.file.myqcloud.com/core/cms.zip &&
-sudo mkdir -p /zkeasoft/cms &&
-sudo unzip -q -o cms.zip -d /zkeasoft/cms &&
-sudo wget http://cdn.zkeasoft.com/core/zkeacms.service &&
-sudo mv zkeacms.service /etc/systemd/system/zkeacms.service -f &&
-sudo systemctl start zkeacms &&
+#!/bin/sh
+sudo curl http://cdn.zkeasoft.com/core/linux.aspnetcore.sh|sh
+sudo apt-get update -y
+sudo apt-get install libunwind-dev -y
+sudo apt-get install zip unzip -y
+sudo rm -rf cms.zip
+echo Downloading ZKEACMS package
+sudo wget -O cms.zip http://zkeasoft-10041459.file.myqcloud.com/core/cms.zip
+sudo mkdir -p /zkeasoft/cms
+sudo unzip -q -o cms.zip -d /zkeasoft/cms
+sudo wget http://cdn.zkeasoft.com/core/zkeacms.service
+sudo mv zkeacms.service /etc/systemd/system/zkeacms.service -f
+sudo systemctl start zkeacms
 sudo systemctl enable zkeacms
