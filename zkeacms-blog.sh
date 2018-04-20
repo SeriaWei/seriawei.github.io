3c930faa41ca875f2902acf94aa0fe96aa69f9aa
Wayne <seriawei@outlook.com>
2018-04-20 12:41:46 +0800

    zkeacms.sh

--- zkeacms-blog.sh
+++ zkeacms-blog.sh
@@ -39,11 +39,24 @@ function installDotnetCore(){
 }
 function installcms(){
     sudo rm -rf cms.zip
-    sudo wget -O cms.zip http://zkeasoft-10041459.file.myqcloud.com/core/blog.zip
+    sudo wget -O cms.zip http://zkeasoft-10041459.file.myqcloud.com/core/cms.zip
     sudo mkdir -p /zkeasoft/cms
     sudo unzip -q -o cms.zip -d /zkeasoft/cms
-    sudo wget http://cdn.zkeasoft.com/core/zkeacms.service
-    sudo mv zkeacms.service /etc/systemd/system/zkeacms.service -f
+
+	sudo echo [Unit] > /etc/systemd/system/zkeacms.service
+	sudo echo Description=ZKEACMS >> /etc/systemd/system/zkeacms.service
+	sudo echo [Service] >> /etc/systemd/system/zkeacms.service
+	sudo echo WorkingDirectory=/zkeasoft/cms >> /etc/systemd/system/zkeacms.service
+	sudo echo ExecStart=/zkeasoft/dotnet/dotnet /zkeasoft/cms/ZKEACMS.WebHost.dll >> /etc/systemd/system/zkeacms.service
+	sudo echo Restart=always >> /etc/systemd/system/zkeacms.service
+	sudo echo RestartSec=10 >> /etc/systemd/system/zkeacms.service
+	sudo echo SyslogIdentifier=zkeacms >> /etc/systemd/system/zkeacms.service
+	sudo echo User=root >> /etc/systemd/system/zkeacms.service
+	sudo echo Environment=ASPNETCORE_ENVIRONMENT=Production >> /etc/systemd/system/zkeacms.service
+	sudo echo Environment=ASPNETCORE_URLS=http://*:80 >> /etc/systemd/system/zkeacms.service
+	sudo echo [Install] >> /etc/systemd/system/zkeacms.service
+	sudo echo WantedBy=multi-user.target >> /etc/systemd/system/zkeacms.service
+    
     sudo systemctl restart zkeacms
     sudo systemctl enable zkeacms
     sudo rm -rf cms.zip
