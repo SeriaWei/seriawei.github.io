108450ae0e3b9f00de1c95f049e0e01428b45452
Wayne <seriawei@outlook.com>
2019-05-09 10:55:13 +0800

    Update zkeacms.sh

--- zkeacms.sh
+++ zkeacms.sh
@@ -30,7 +30,7 @@ function installDotnetCore(){
 
     sudo rm -rf /zkeasoft/dotnet 
     sudo mkdir -p /zkeasoft/dotnet
-    sudo wget -O dotnet-hosting.tar.gz http://www.zkea.net/windows-hosting-bundle-installer
+    sudo wget -O dotnet-hosting.tar.gz http://www.zkea.net/dotnet-runtime-linux
     sudo tar zxf dotnet-hosting.tar.gz -C /zkeasoft/dotnet
     sudo rm -rf dotnet-hosting.tar.gz
 }
